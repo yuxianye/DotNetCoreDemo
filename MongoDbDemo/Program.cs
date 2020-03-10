2b0bfc6db50e1a1947937d332d7ade0827ed1f78
yuxianye <yuxianye@163.com>
2020-03-10 16:59:57 +0800

    增加MongoDB的例子，查询待2实现

---
+++ MongoDbDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+namespace MongoDbDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new MongoDB.Driver.MongoClient("mongodb://192.168.1.117:27017");
+
+            var database = client.GetDatabase("test");
+
+            var document = new BsonDocument
+{
+    { "yuxianye", "MongoDB" },
+    { "name", "MongoDB" },
+    { "type", "Database" },
+    { "count", 1 },
+    { "info", new BsonDocument
+        {
+            { "x", 203 },
+            { "y", 102 }
+        }}
+};
+
+
+            var collection = database.GetCollection<MongoDB.Bson.BsonDocument>("test");
+            collection.InsertOne(document);
+            var count = collection.CountDocuments(new BsonDocument());
+
+            //Console.WriteLine(collection.ToJson<BsonDocument>());
+
+            var vv = collection.Find(new BsonDocument()).ToList();
+
+            foreach (var v in vv)
+            {
+                Console.WriteLine($"{v.ToString()}");
+            }
+
+            Console.WriteLine($"数量{count}");
+
+            var document2 = collection.FindSync(new BsonDocument());
+            Console.WriteLine(document2.ToString());
+
+            Console.ReadLine();
+
+        }
+    }
+}
